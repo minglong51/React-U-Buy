3150eb13c79d0bb7c0ddf33bad87e8bee5c17ddb
Sze Wai <szewai@Szes-MacBook-Pro.local>
2021-02-16 21:45:05 -0800

    Added network call

--- react-u-buy/src/component/LikedGames.js
+++ react-u-buy/src/component/LikedGames.js
@@ -1,16 +1,31 @@
 import React, {useState, Component} from 'react';
 import 'antd/dist/antd.css';
 import {List, Avatar } from 'antd';
+import axios from 'axios';
 
 
 
 
 class LikedGames extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {likedGames: []};
+  }
 
+  componentDidMount = () => {
+    // http://optubuy.us-east-2.elasticbeanstalk.com/products?page=4&page_size=3
+    const url = '/products?page=4&page_size=3';
+    axios.get(url)
+       .then(response => {
+           console.log(response)
+       })
+       .catch(error => {
+           console.log('err in fetch satellite -> ', error);
+       })
 
+  }
 
-
-    render() {
+  render() {
         const data = [
             {
               title: 'Ant Design Title 1',
