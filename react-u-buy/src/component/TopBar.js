8c53ccac77ae7d11561672b742fed3492116657e
Sze Wai <szewai@Szes-MacBook-Pro.local>
2021-02-19 08:20:36 -0800

    Edit CSS for top bar

--- react-u-buy/src/component/TopBar.js
+++ react-u-buy/src/component/TopBar.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import { Icon } from 'antd';
+import heart  from '../assets/heart.svg';
 
 class TopBar extends Component {
     render() {
         return (
             <header className="App-header">
-                <span className="App-title">Opt-U-Buy Application</span>
+
 
                 {this.props.isLoggedIn ?
                     <a className="logout" onClick={this.props.handleLogout} >
                         <Icon type="logout"/>{' '}Logout
                     </a> : null }
+                <a className="heart">
+                        <Icon type="heart"/>{' '}Liked Games
+                </a>
+
+         
+               
 
             </header>
         );
