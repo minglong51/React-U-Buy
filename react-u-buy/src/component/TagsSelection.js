27a5248d071c121a45ff4fc8eae66f7803cbea42
Sze Wai <szewai@Szes-MacBook-Pro.local>
2021-02-19 18:31:07 -0800

    Touch up on tags page and header

--- react-u-buy/src/component/TagsSelection.js
+++ react-u-buy/src/component/TagsSelection.js
@@ -20,15 +20,17 @@ class TagsSelection extends Component {
 
                 <div>
                     <Tag color="magenta" className="Tag">magenta sfaffsfe eaf</Tag>
-                    <Tag color="magenta" className="Tag">magentagsgsgvgsvs</Tag>
-                    <Tag color="magenta" className="Tag">magen sfsg  stgsta</Tag>
-                    <Tag color="magenta" className="Tag">mageg snta</Tag>
-                    <Tag color="magenta" className="Tag">mage sfssnta</Tag>
-                    <Tag color="magenta" className="Tag">magenta</Tag>
-                    <Tag color="magenta" className="Tag">magenta</Tag>
-                    <Tag color="magenta" className="Tag">magenta</Tag>
+                    <Tag color="blue" className="Tag">magentagsgsgvgsvs</Tag>
+                    <Tag color="yellow" className="Tag">magen sfsg  stgsta</Tag>
+                    <Tag color="blue" className="Tag">mageg snta</Tag>
+                    <Tag color="green" className="Tag">mage sfssnta</Tag>
+                    <Tag color="red" className="Tag">magenta</Tag>
+                    <Tag color="orange" className="Tag">magenta</Tag>
+                    <Tag color="purple" className="Tag">magenta</Tag>
                 </div>
-
+                <div className="recommendBtnContainer">
+                    <button className="recommendBtn">Recommend!</button>
+                </div>             
                 </div>
                 
              </div>    
--- react-u-buy/src/component/TopBar.js
+++ react-u-buy/src/component/TopBar.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import { Icon } from 'antd';
 import heart  from '../assets/heart.svg';
-
+import controller2  from '../assets/controller2.svg';
 class TopBar extends Component {
     render() {
         return (
             <header className="App-header">
-
-
+                <div className="controller">
+                <img src={controller2} className="App-logo" alt="logo" />
+                </div>
                 {this.props.isLoggedIn ?
                     <a className="logout" onClick={this.props.handleLogout} >
                         <Icon type="logout"/>{' '}Logout
@@ -16,7 +17,6 @@ class TopBar extends Component {
                       <a className="heart">
                       <Icon type="heart"/>{' '}Liked Games
                      </a>: null }
-
             </header>
         );
     }
